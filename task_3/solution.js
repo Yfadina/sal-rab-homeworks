884b2dd8b6951b80fe4af3f1e4c7bbb7839badba
Ju7ia <ju7ia@mail.ru>
2023-05-18 19:28:14 +0300

    TASK 3 DONE

--- task_3/solution.js
+++ task_3/solution.js
@@ -10,14 +10,14 @@ function renderCartItem(item) {
     // price - цена
     // count - количество
     
-    let itemCountText = ``;
+    let itemCountText = "";
     itemCountText = itemCountText + product.count;
-    itemCountText = itemCountText + ` * `;
+    itemCountText = itemCountText + " × ";
     itemCountText = itemCountText + product.price;
-    itemCountText = itemCountText + ` ₽ = `;
+    itemCountText = itemCountText + " ₽ = ";
     let sum = product.count * product.price;
     itemCountText = itemCountText + sum;
-    itemCountText = itemCountText + ` ₽ `;
+    itemCountText = itemCountText + " ₽";
     
 
     // Конец решения задания №3.1.
